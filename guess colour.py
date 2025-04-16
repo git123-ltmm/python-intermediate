1b82ae24eb22b13429fbe6c580e0443583241607
git123-ltmm <yaashnaickeer@gmail.com>
2025-04-16 12:28:57 +0800

    Add files via upload

---
+++ guess colour.py
@@ -0,0 +1,24 @@
+import random
+
+def main():
+    """Start a colour guessing game.""" 
+    print ("Guess the colour!")
+    rainbow = [
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "blue",
+        "indigo",
+        "violet",
+    ]
+    x = random.choice(rainbow)
+    guess = None
+    while x != guess:
+        guess = str(input ("What colour am I thinking of? "))
+        if x == guess:
+            print("You guessed {}. Congratulations you got it right!".format(guess))
+            break
+        else:
+            print("You guessed {}. Unfortunately its wrong. Try again!".format(guess))
+main()
